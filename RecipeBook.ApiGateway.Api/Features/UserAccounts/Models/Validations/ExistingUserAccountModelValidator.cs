1299cb033c2876aaef092024ce09ccd569751273
Peter Jones <peter.jones@loyaltylogistix.com>
2021-10-07 14:45:51 +0100

    Added FluentValidations

---
+++ RecipeBook.ApiGateway.Api/Features/UserAccounts/Models/Validations/ExistingUserAccountModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace RecipeBook.ApiGateway.Api.Features.UserAccounts.Models.Validations
+{
+    public class ExistingUserAccountModelValidator : AbstractValidator<ExistingUserAccountModel>
+    {
+        public ExistingUserAccountModelValidator()
+        {
+            Include(new UserAccountModelValidator());
+
+            RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+}
---
+++ RecipeBook.ApiGateway.Api/Features/UserAccounts/Models/Validations/UserAccountModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace RecipeBook.ApiGateway.Api.Features.UserAccounts.Models.Validations
+{
+    public class UserAccountModelValidator : AbstractValidator<UserAccountModel>
+    {
+        public UserAccountModelValidator()
+        {
+            RuleFor(x => x.Role).NotEmpty();
+        }
+    }
+}
